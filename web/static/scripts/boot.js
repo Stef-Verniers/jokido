8b720a681e73f52f0662b6c70455b0113031525b
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-10-21 22:34:10 +0200

    Made changes

--- web/static/scripts/boot.js
+++ web/static/scripts/boot.js
@@ -7,11 +7,9 @@ function renderHeader() {
     const screenHeader = document.querySelector('.screen-header')
 
     if (window.innerWidth < 762) {
-        console.log('mobile')
         mobileHeader.style.display = 'block'
         screenHeader.style.display = 'none'
     } else {
-        console.log('screen')
         mobileHeader.style.display = 'none'
         screenHeader.style.display = 'block'
     }
